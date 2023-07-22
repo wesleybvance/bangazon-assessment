5da9677dc12d546282e28e9865b0bcc2a3389b46
Wesley Vance <wslyvnce@gmail.com>
2023-07-22 11:01:34 -0500

    added sample data completely

--- thredsapi/models/order.py
+++ thredsapi/models/order.py
@@ -7,5 +7,5 @@ class Order(models.Model):
 
     is_shipped = models.BooleanField(default=True)
     is_open = models.BooleanField(default=True)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2)
+    order_total = models.DecimalField(null=True, max_digits=10, decimal_places=2)
     payment_type = models.CharField(max_length=100)
